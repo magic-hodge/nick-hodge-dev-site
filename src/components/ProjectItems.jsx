4502a162d273c1f11f6435790847c9b4fac6099d
magic-hodge <nick.m.hodge@gmail.com>
2020-10-03 03:32:16 -0700

    added current projects to ProjectItems array. Ready to map inside the ProjectsPage component I create.

---
+++ src/components/ProjectItems.jsx
@@ -0,0 +1,51 @@
+export const ProjectItems = [
+    {
+        title: `Domino's Delivery App`,
+        techStack: 'MongoDB • Express.js • React.js • Node.js',
+        description: `A mobile-first Domino's app that dynamically lists apartment maps & gate codes and allows users to submit new locations to the database.`,
+        host: 'Heroku',
+        imageURL: './images/DominosApp.PNG'
+    },
+    {
+        title: `Hot Beatz! Music Player App`,
+        techStack: 'Vue.js',
+        description: `A mobile-friendly music-player app built using Vue.`,
+        host: 'Github',
+        imageURL: './images/hotBeatz.PNG'
+    },
+    {
+        title: `Open Weather API Journal App`,
+        techStack: 'JavaScript • Express.js • Node.js',
+        description: `A weather journal app utilizing the Open Weather API and JavaScript to route JSON data and dynamically update the UI based on user input.`,
+        host: 'Glitch',
+        imageURL: './images/weatherJournalApp2.PNG'
+    },
+    {
+        title: `Dynamic Landing Page`,
+        techStack: 'HTML • CSS • JavaScript',
+        description: `A landing page project featuring a dynamically updating navigation menu based on content added to the page - with styling added for states that are active and in view.`,
+        host: 'Github',
+        imageURL: './images/dynamicLandingPage.PNG'
+    },
+    {
+        title: `Nick Hodge Circus Blog`,
+        techStack: 'HTML • CSS',
+        description: `HTML & CSS project using Flexbox & Grid to create a responsive and mobile-friendly blog website.`,
+        host: 'Github',
+        imageURL: './images/NHCBlog.PNG'
+    },
+    {
+        title: `Pixel Art Maker`,
+        techStack: 'HTML • CSS • JavaScript',
+        description: `A simple, user-friendly pixel-art maker utilizing JavaScript for-loops to create the grid-size.`,
+        host: 'Github',
+        imageURL: './images/pixelArtMaker.PNG'
+    },
+    {
+        title: `Text-Based Adventure Game`,
+        techStack: 'Python',
+        description: `A text-based adventure game developed in Python including randomness, loops to check for valid input, and alternate endings depending on the player's choices.`,
+        host: 'Github',
+        imageURL: './images/adventureGame.PNG'
+    }
+]
