5c3f5858df32eefcf715f3ea9e0ad29be44f8c65
magic-hodge <nick.m.hodge@gmail.com>
2020-10-07 22:42:18 -0700

    deployed to gh-pages successfully.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import ProjectsPage from './ProjectsPage';
